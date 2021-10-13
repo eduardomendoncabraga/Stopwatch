afbf05521e392a55e44c2107684f12f57208c0c1
Eduardo <eduardomendoncabraga@gmail.com>
2021-10-13 15:36:19 -0300

    Get option and value

--- Program.cs
+++ Program.cs
@@ -19,8 +19,11 @@ namespace Stopwatch
       Console.WriteLine("Quanto tempo deseja contar?");
 
       string data = Console.ReadLine().ToLower();
-      Console.WriteLine(data);
+      char type = char.Parse(data.Substring(data.Length - 1, 1));
+      int time = int.Parse(data.Substring(0, data.Length - 1));
 
+      Console.WriteLine(type);
+      Console.WriteLine(time);
     }
 
     static void Start(int time)
