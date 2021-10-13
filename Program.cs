7b78c7faec451383e83910c2c4556f3df9e93590
Eduardo <eduardomendoncabraga@gmail.com>
2021-10-13 15:16:49 -0300

    Definition struct stopwatch program

--- Program.cs
+++ Program.cs
@@ -2,11 +2,22 @@
 
 namespace Stopwatch
 {
-    internal class Program
+  internal class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      Console.WriteLine("Hello World!");
     }
+
+    static void Start()
+    {
+      int time = 10;
+      int currentTime = 0;
+
+      while (currentTime != time)
+      {
+        currentTime++;
+      }
+    }
+  }
 }
